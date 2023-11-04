b853b3e42fb7afceb277082369fd5be7489e68c5
krutibagwe <83567614+krutibagwe@users.noreply.github.com>
2023-11-05 02:35:00 +0530

    Update applyTwice.hs

--- applyTwice.hs
+++ applyTwice.hs
@@ -1,6 +1,6 @@
 applyTwice::(a->a)->(a->a)->a->a
-applyTwice f1 f2 x = f2 (f1 x)
+applyTwice f1 f2 x= f1(f2 x)
 
-main = do
-    putStrLn"Hello World!"
-    print(applyTwice(+5)(*10)8)
+main=do
+    putStrLn "Hello World!"
+    print(applyTwice(*10)(+5)8)
