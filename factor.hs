a00e1ab0496cc2139fd21d45a0802ea80f67b254
krutibagwe <83567614+krutibagwe@users.noreply.github.com>
2023-11-06 00:05:42 +0530

    Update factor.hs

--- factor.hs
+++ factor.hs
@@ -1,5 +1,5 @@
 factor::Int->[Int]
-factor n =[x|x<-[1..n], (n `mod` x ==0) && (x/=0) && (x/=n)]
+factor n =[x|x<-[1..n], (n `mod` x ==0) && (x/=1) && (x/=n)]
 
 main = do
     putStrLn "Enter the number n: "
